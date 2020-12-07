8cae8e291cae60d3b23a6ce8bd29e596cc8f9a67
parfait <parfaitebobisse@gmail.com>
2020-12-07 07:38:16 +0100

    page dejeuner faite

--- storage/framework/views/1116bdd173309ec97738ce46d3a868e20ce62435.php
+++ storage/framework/views/1116bdd173309ec97738ce46d3a868e20ce62435.php
@@ -12,54 +12,69 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale = 1.0"/>
-<link rel="shortcut icon" type="image/png" href="{{asset('design/assets/uploads/2019/04/favicon.png')}}"/>
+<link rel="shortcut icon" type="image/png" href="<?php echo e(asset('design/assets/uploads/2019/04/favicon.png')); ?>"/>
 <title>Evénement</title>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link rel='stylesheet' href='{{asset('design/assets/css/fonts5223.css')}}' type='text/css' media='all' />
-<link rel='stylesheet' href='{{asset('design/assets/css/dejeuner.css')}}' type='text/css' media='all' />
-<link rel='stylesheet' href='{{asset('design/assets/css/bootstrap.min5223.css')}}' type='text/css' media='all' />
-<link rel='stylesheet' href='{{asset('design/assets/css/animate5223.css')}}' type='text/css' media='all' />
-<link rel='stylesheet' href='{{asset('design/assets/css/flexslider5223.css')}}' type='text/css' media='all' />
-<link rel='stylesheet'  href='{{asset('design/assets/css/dist/block-library/style.min5223.css')}}' type='text/css' media='all' />
-<link rel='stylesheet' href='{{asset('design/assets/css/dist/block-library/theme.min5223.css')}}' type='text/css' media='all' />
-<link rel='stylesheet' href='{{asset('design/assets/style5224.css')}}' type='text/css' media='all' />
-<link rel='stylesheet' href="{{asset('design/assets/style5223.css')}}" type='text/css' media='all'/>
-<link rel='stylesheet'  href='{{asset('design/assets/print5223.css')}}' type='text/css' media='print' />
+<link rel='stylesheet' href='<?php echo e(asset('design/assets/css/fonts5223.css')); ?>' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo e(asset('design/assets/css/dejeuner.css')); ?>' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo e(asset('design/assets/css/bootstrap.min5223.css')); ?>' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo e(asset('design/assets/css/animate5223.css')); ?>' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo e(asset('design/assets/css/flexslider5223.css')); ?>' type='text/css' media='all' />
+<link rel='stylesheet'  href='<?php echo e(asset('design/assets/css/dist/block-library/style.min5223.css')); ?>' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo e(asset('design/assets/css/dist/block-library/theme.min5223.css')); ?>' type='text/css' media='all' />
+<link rel='stylesheet' href='<?php echo e(asset('design/assets/style5224.css')); ?>' type='text/css' media='all' />
+<link rel='stylesheet' href="<?php echo e(asset('design/assets/style5223.css')); ?>" type='text/css' media='all'/>
+<link rel='stylesheet'  href='<?php echo e(asset('design/assets/print5223.css')); ?>' type='text/css' media='print' />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" />
     
-<link rel="stylesheet" href="{{asset('design/parfait_integration/css/header_respon.css')}}">
-<link rel="stylesheet" href="{{asset('design/parfait_integration/css/dejeuner.css')}}">
+<link rel="stylesheet" href="<?php echo e(asset('design/parfait_integration/css/header_respon.css')); ?>">
+<link rel="stylesheet" href="<?php echo e(asset('design/parfait_integration/css/dejeuner.css')); ?>">
 
-<script type='text/javascript' src='{{asset('design/assets/js/popper.min5223.js')}}'></script>
-<script type='text/javascript' src='{{asset('design/assets/js/jquery/jquery4a5f.js')}}'></script>
-<script type='text/javascript' src='{{asset('design/assets/js/jquery/jquery-migrate.min330a.js')}}'></script>
-<script type='text/javascript' src='{{asset('design/assets/plugins/video-popup/js/YouTubePopUp.jquery0d3f.js')}}'></script>
-<script type='text/javascript' src='{{asset('design/assets/plugins/video-popup/js/YouTubePopUp0d3f.js')}}'></script>
-<script type='text/javascript' src='{{asset('design/assets/js/bootstrap.min5223.js')}}'></script>
-<script type='text/javascript' src='{{asset('design/assets/js/jquery.flexslider5223.js')}}'></script>
-<script type='text/javascript' src='{{asset('design/assets/js/custom5223.js')}}'></script>
-@livewireStyles
+<script type='text/javascript' src='<?php echo e(asset('design/assets/js/popper.min5223.js')); ?>'></script>
+<script type='text/javascript' src='<?php echo e(asset('design/assets/js/jquery/jquery4a5f.js')); ?>'></script>
+<script type='text/javascript' src='<?php echo e(asset('design/assets/js/jquery/jquery-migrate.min330a.js')); ?>'></script>
+<script type='text/javascript' src='<?php echo e(asset('design/assets/plugins/video-popup/js/YouTubePopUp.jquery0d3f.js')); ?>'></script>
+<script type='text/javascript' src='<?php echo e(asset('design/assets/plugins/video-popup/js/YouTubePopUp0d3f.js')); ?>'></script>
+<script type='text/javascript' src='<?php echo e(asset('design/assets/js/bootstrap.min5223.js')); ?>'></script>
+<script type='text/javascript' src='<?php echo e(asset('design/assets/js/jquery.flexslider5223.js')); ?>'></script>
+<script type='text/javascript' src='<?php echo e(asset('design/assets/js/custom5223.js')); ?>'></script>
+<?php echo \Livewire\Livewire::styles(); ?>
+
 </head>
 <body>
-@include('partials.site_menu')
+<?php echo $__env->make('partials.site_menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
     <header class="bg-white">
-        @livewire('head',['viewname' => 'Evénement'])
+        <?php
+if (! isset($_instance)) {
+    $dom = \Livewire\Livewire::mount('head',['viewname' => 'Evénement'])->dom;
+} elseif ($_instance->childHasBeenRendered('E3SWJHj')) {
+    $componentId = $_instance->getRenderedChildComponentId('E3SWJHj');
+    $componentTag = $_instance->getRenderedChildComponentTagName('E3SWJHj');
+    $dom = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('E3SWJHj');
+} else {
+    $response = \Livewire\Livewire::mount('head',['viewname' => 'Evénement']);
+    $dom = $response->dom;
+    $_instance->logRenderedChild('E3SWJHj', $response->id, \Livewire\Livewire::getRootElementTagName($dom));
+}
+echo $dom;
+?>
     </header>
         <div class="container_card" style="margin-bottom: 13vh;   margin-top: 75px;">
-            @foreach ($events as $event)
+            <?php $__currentLoopData = $events; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $event): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <div class="card_fond">
                 <div class="container_photo_fond">
-                    <img src="{{asset('storage/'.$event->image)}}" alt="{{$event->titre}}">
+                    <img src="<?php echo e(asset('storage/'.$event->image)); ?>" alt="<?php echo e($event->titre); ?>">
                 </div>
-                <h2 class="titre_fond">{{$event->titre}}</h2>
-                <p class="paragraphe_fond">{!! $event->descriptif !!}</p>
+                <h2 class="titre_fond"><?php echo e($event->titre); ?></h2>
+                <p class="paragraphe_fond"><?php echo $event->descriptif; ?></p>
                 <div class="container_bouton_learn">
-                    <a href="#" class="learn_button bouton_africkup anul_lien" data-type="{{$event->type}}" data-toggle="modal"
-                         data-target="#@if ($event->type == 'organisation'){{"modal-dejeuner"}}@else{{"modal-dejeuner-intermediation"}}@endif">Learn more</a>
+                    <a href="#" class="learn_button bouton_africkup anul_lien" data-type="<?php echo e($event->type); ?>" data-toggle="modal"
+                         data-target="#<?php if($event->type == 'organisation'): ?><?php echo e("modal-dejeuner"); ?><?php else: ?><?php echo e("modal-dejeuner-intermediation"); ?><?php endif; ?>">Learn more</a>
                 </div>
             </div>  
-            @endforeach
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     </div>
 
     <!-- Modal 1 -->
@@ -69,11 +84,11 @@
                 <div class="container">
                     <div class="text">
                         Contactez nous</div>
-                    <form action="{{route('event_request',['locale' => App::getLocale()])}}" method="POST">
-                    @csrf
+                    <form action="<?php echo e(route('event_request',['locale' => App::getLocale()])); ?>" method="POST">
+                    <?php echo csrf_field(); ?>
                         <div class="form-row">
                             <input hidden value="" id="event_type" name="event_type"/>
-                            <input hidden value="{{$service->slug}}" id="slug" name="slug"/>
+                            <input hidden value="<?php echo e($service->slug); ?>" id="slug" name="slug"/>
                             <div class="input-data">
                                 <input type="text" name="nom" required>
                                 <div class="underline">
@@ -106,13 +121,13 @@
                             <div class="input-data">
 								<select id="pays" name="pays">
                   <option value="">Choisir un pays</option>
-									@foreach($all_countries as $country) 
-										<option value="{{$country->name->common}}">{!! $country->name->common !!}</option>
-									@endforeach
+									<?php $__currentLoopData = $all_countries; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $country): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?> 
+										<option value="<?php echo e($country->name->common); ?>"><?php echo $country->name->common; ?></option>
+									<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 								</select>
                                 <div class="underline">
                                 </div>
-                                {{-- <label for="">Pays</label> --}}
+                                
                             </div>
                             <div class="input-data">
                                 <input type="text" name="ville" required>
@@ -183,11 +198,11 @@
                 <div class="container">
                     <div class="text">
                         Contactez nous</div>
-                    <form action="{{route('event_request',['locale' => App::getLocale()])}}" method="POST" enctype="multipart/form-data">
-                    @csrf
+                    <form action="<?php echo e(route('event_request',['locale' => App::getLocale()])); ?>" method="POST" enctype="multipart/form-data">
+                    <?php echo csrf_field(); ?>
                         <div class="form-row">
                             <input hidden value="" id="event_type" name="event_type"/>
-                            <input hidden value="{{$service->slug}}" id="slug" name="slug"/>
+                            <input hidden value="<?php echo e($service->slug); ?>" id="slug" name="slug"/>
                             <div class="input-data">
                                 <input type="text" name="nom" required>
                                 <div class="underline">
@@ -220,13 +235,13 @@
                             <div class="input-data">
 								<select id="pays" name="pays">
                                     <option value="">Choisir un pays</option>
-									@foreach($all_countries as $country) 
-										<option value="{{$country->name->common}}">{!! $country->name->common !!}</option>
-									@endforeach
+									<?php $__currentLoopData = $all_countries; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $country): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?> 
+										<option value="<?php echo e($country->name->common); ?>"><?php echo $country->name->common; ?></option>
+									<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 								</select>
                                 <div class="underline">
                                 </div>
-                                {{-- <label for="">Pays</label> --}}
+                                
                             </div>
                             <div class="input-data">
                                 <input type="text" name="ville" required>
@@ -282,13 +297,27 @@
 
 
   <footer class="bg-white2">
-    @livewire('footer')
+    <?php
+if (! isset($_instance)) {
+    $dom = \Livewire\Livewire::mount('footer')->dom;
+} elseif ($_instance->childHasBeenRendered('OKI7TK4')) {
+    $componentId = $_instance->getRenderedChildComponentId('OKI7TK4');
+    $componentTag = $_instance->getRenderedChildComponentTagName('OKI7TK4');
+    $dom = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('OKI7TK4');
+} else {
+    $response = \Livewire\Livewire::mount('footer');
+    $dom = $response->dom;
+    $_instance->logRenderedChild('OKI7TK4', $response->id, \Livewire\Livewire::getRootElementTagName($dom));
+}
+echo $dom;
+?>
   </footer>
   <script src="../cdn.jsdelivr.net/npm/bootstrap-select%401.13.9/dist/js/bootstrap-select.min.js"></script>
-  <script type='text/javascript' src='{{asset('design/assets/plugins/responsive-youtube-vimeo-popup/assets/js/wp-video-popup5223.js')}}'></script>
-  <script type='text/javascript' src='{{asset('design/assets/js/priority-menu4963.js')}}'></script>
-  <script type='text/javascript' src='{{asset('design/assets/plugins/sassy-social-share/public/js/sassy-social-share-publicabd4.js')}}'></script>
-  <script type='text/javascript' src='{{asset('design/assets/js/wp-embed.min5223.js')}}'></script>
+  <script type='text/javascript' src='<?php echo e(asset('design/assets/plugins/responsive-youtube-vimeo-popup/assets/js/wp-video-popup5223.js')); ?>'></script>
+  <script type='text/javascript' src='<?php echo e(asset('design/assets/js/priority-menu4963.js')); ?>'></script>
+  <script type='text/javascript' src='<?php echo e(asset('design/assets/plugins/sassy-social-share/public/js/sassy-social-share-publicabd4.js')); ?>'></script>
+  <script type='text/javascript' src='<?php echo e(asset('design/assets/js/wp-embed.min5223.js')); ?>'></script>
   <script>
       jQuery(document).ready(function(){
           if($('.input-data select').val() != ""){
@@ -297,10 +326,11 @@
           }
       });
   </script>
-    @toastr_js
-    @toastr_render
-@livewireScripts
-	@stack('scripts')
+    <?php echo toastr_js(); ?>
+    <?php echo app('toastr')->render(); ?>
+<?php echo \Livewire\Livewire::scripts(); ?>
+
+	<?php echo $__env->yieldPushContent('scripts'); ?>
 </body>
 
-</html>
+</html><?php /**PATH C:\wamp64\www\dashboard-model\resources\views/services/evenement.blade.php ENDPATH**/ ?>
